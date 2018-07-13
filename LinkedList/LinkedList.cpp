d0f9adb1ca3b1fad5f2b9524eb63383a2c48396b
Vamseedhar <vamseedhar.reddyvari@gmail.com>
2018-07-12 21:13:45 -0500

    delete not working

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -33,9 +33,66 @@ class MyLinkedList{
 
     }
 
-    void delete_node(LLNode* prev_ptr){
+    LLNode* delete_node(LLNode* prev_ptr){
+        LLNode *dangle = prev_ptr->next_ptr;
         LLNode *tmp_ptr = prev_ptr->next_ptr->next_ptr;
-        prev_ptr = tmp_ptr;
+        prev_ptr->next_ptr = tmp_ptr;
+        cout<<"dangle: "<<dangle<<", value: "<<&dangle->value<<endl;
+        delete dangle;
+        cout<<"dangle: "<<dangle<<", value: "<<&dangle->value<<endl;
+        return dangle;
+    }
+
+
+    void print(){
+        LLNode* iter_ptr = llist;
+        while(iter_ptr !=NULL){
+            cout<<iter_ptr->value<<" ";
+            iter_ptr = iter_ptr->next_ptr;
+        }
+        cout<<endl;
+
+    }
+
+    LLNode* head(){
+        return llist;
+    }
+
+};
+
+
+class LinkedListUniquePtr{
+
+
+    private:
+        LLNode* llist;
+    public:
+    //constructor
+    LinkedListUniquePtr() {
+        llist = NULL;
+    }
+
+    LLNode* push_front(int elem){
+        LLNode * newNode = new LLNode;
+        newNode->value = elem;
+        newNode->next_ptr = llist;
+        llist = newNode;
+        return llist;
+
+    }
+
+    LLNode* delete_node(LLNode* prev_ptr){
+        LLNode *dangle = prev_ptr->next_ptr;
+        LLNode *tmp_ptr = prev_ptr->next_ptr->next_ptr;
+        prev_ptr->next_ptr = tmp_ptr;
+        return dangle;
+    }
+
+
+    void delete_node_through_head(LLNode* &prev_ptr){
+        LLNode *tmp_ptr = prev_ptr->next_ptr->next_ptr;
+        prev_ptr->next_ptr = tmp_ptr;
+
     }
 
     void print(){
@@ -64,8 +121,9 @@ int main(){
     list->push_front(47);
     list->push_front(48);
     list->print();
-    list->delete_node(list->head()->next_ptr);
+    LLNode* del = list->delete_node(list->head()->next_ptr);
     list->print();
+    cout<<"Deleted Value: "<<del->value<<endl;
     return 0;
 
 }

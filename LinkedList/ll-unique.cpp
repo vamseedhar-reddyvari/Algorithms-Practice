8eb3eb2c9be6c908856e082fa91a233cd704bb36
Vamseedhar <vamseedhar.reddyvari@gmail.com>
2018-07-26 07:28:10 -0500

    working

--- LinkedList/ll-unique.cpp
+++ LinkedList/ll-unique.cpp
@@ -3,14 +3,12 @@
 
 using namespace std;
 
-
 struct LLNode{
     int value;
     unique_ptr<LLNode> next_ptr;
 };
 
 class LinkedListUniquePtr{
-
 
     private:
         unique_ptr<LLNode> head_ptr;
@@ -20,6 +18,21 @@ class LinkedListUniquePtr{
         head_ptr = NULL;
     }
 
+    void push_back(int elem){
+        // create a new elem 
+        unique_ptr<LLNode> newNode (new LLNode());
+        newNode->value = elem;
+        newNode->next_ptr = NULL;
+        //traverse to the end
+        unique_ptr<LLNode> *tmp_ptr = &head_ptr;
+        while( (*tmp_ptr)!= NULL){
+            tmp_ptr = &(*tmp_ptr)->next_ptr;
+        }
+        //move ownership
+        (*tmp_ptr).reset();
+        (*tmp_ptr) = move(newNode);
+        
+    }
 
     void push_front(int elem){
         unique_ptr<LLNode> newNode(new LLNode());
@@ -40,10 +53,17 @@ class LinkedListUniquePtr{
             cout<<curr_ptr->value<<", ";
             print(curr_ptr->next_ptr);
         }
-
     }
 
     void print_trusted(){
+        unique_ptr<LLNode> *curr_ptr = &head_ptr;
+        while( (*curr_ptr) != NULL){
+            cout<<(*curr_ptr)->value<<", ";
+            curr_ptr = &(*curr_ptr)->next_ptr;
+        }
+        cout<<endl;
+    }
+    void print_trusted2(){
         LLNode *curr_ptr = head_ptr.get();
         while(curr_ptr != NULL){
             cout<<curr_ptr->value<<", ";
@@ -61,6 +81,9 @@ int main(){
     list->push_front(8);
     list->push_front(12);
     list->push_front(15);
+    list->print_trusted();
+    list->push_back(27);
+    list->push_back(9);
     list->print_trusted();
     return 0;
 

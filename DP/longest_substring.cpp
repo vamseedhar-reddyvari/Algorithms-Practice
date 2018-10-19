b5ba774f49d00333f5b73fa8c4a033cc3a7678cc
Vamseedhar <vamseedhar.reddyvari@gmail.com>
2018-10-19 09:28:38 -0500

    removed extra loop

--- DP/longest_substring.cpp
+++ DP/longest_substring.cpp
@@ -22,15 +22,21 @@ string longest_common_substr(string s1, string s2){
         suffix_mtx[0][i] = (s2[i] == s1[0]);
     }
 
-    // Recursively construct 
+    // Recursively construct
+    int p1 = 0;
+    int max_value = 0;
     for(int i=1; i <p ; i++){
         for(int j =1; j<q; j++){
             if(s1[i] != s2[j]) suffix_mtx[i][j]  = 0;
-            else {suffix_mtx[i][j]  = suffix_mtx[i-1][j-1]  +1;}
+            else {
+              suffix_mtx[i][j]  = suffix_mtx[i-1][j-1]  +1;
+              if (suffix_mtx[i][j] >=  max_value){
+                  max_value = suffix_mtx[i][j];
+                  p1 = i;
+            }
+          }
         }
     }
-
-    
     // print suffix mtx
     for(int i=0; i <p ; i++){
         for(int j =0; j<q; j++){
@@ -38,31 +44,15 @@ string longest_common_substr(string s1, string s2){
         }
         cout<<endl;
     }
-    // Find the index of largest 
-    int p1 = 0;
-    int q1 = 0;
-    int max_value = 0;
-    for(int i=0; i <p ; i++){
-        for(int j =0; j<q; j++){
-            if (suffix_mtx[i][j] >=  max_value){
-                max_value = suffix_mtx[i][j];
-                p1 = i;
-                q1 = j;
-            }
-        }
-    }
-
-    cout<< "p1: "<< p1<< " q1:" <<q1<< " max_value: "<<max_value <<endl;
+    cout<< "p1: "<< p1<<  " max_value: "<<max_value <<endl;
     string longest_substring = s1.substr(p1-max_value+1, max_value);
     return longest_substring;
 }
 
 
-
-
 int main(){
-    string s1 = "pridhvis";
-    string s2 = "pridhvi";
+    string s1 = "kavya";
+    string s2 = "yaya";
     cout<< longest_common_substr(s1,s2)<<endl;;
 
 }
